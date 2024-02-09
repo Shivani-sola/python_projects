669aaf5c2e45aa3326a73a3089537ace3e466018
Shivani-sola <149178251+Shivani-sola@users.noreply.github.com>
2024-02-09 09:13:00 +0530

    operators module

---
+++ operatores-task2.py
@@ -0,0 +1,66 @@
+num1 = 10
+num2 = 20
+add = num1 + num2
+print(add)
+add2 = num2-num1
+print(add2)
+add3 = num1*num2
+print(add3)
+add4 =num1/num2
+print(add4)
+add5 =num1%num2
+print(add5)
+add6 =num1**num2
+print(add6)
+#task-2
+is_sunny = True
+is_warm = False
+if is_sunny and is_warm:
+    print("It's a nice day!")
+else:
+    print("It's not a nice day!")
+if is_sunny or is_warm:
+    print("It's not too bad!")
+else:
+    print("It's terrible!")
+if not is_sunny:
+    print("It's cloudy!")
+else:
+    print("It's sunny!")
+#task-3
+    a = 10
+b = 10
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+a = 10
+b = 20    
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+if a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is not less than or equal to b")
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("a is not greater than or equal to b")
+#task-4 concatination of strings
+    first_name = 'shivani'
+last_name ='sola'
+full_name = first_name +" "+last_name
+print(full_name)
+#..
+name = "Innomatics"
+print(name * 5)
