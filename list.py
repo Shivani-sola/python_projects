ce6a45fd900701507b8964d6a9277f8f9f9e4a2c
Shivani-sola <149178251+Shivani-sola@users.noreply.github.com>
2024-03-02 20:22:44 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+# task-1
+number =[]
+number.append(1)
+number.append(2)
+number.append(3)
+print(number)
+number[1]=4
+print(number)
+
+
+# In[4]:
+
+
+#task-2
+fruits = ['apple','banana','orange','mango','kiwi']
+citrus_fruits = fruits[2:4]
+tropical_fruits = fruits[1::3]
+combine = citrus_fruits + tropical_fruits
+print(combine)
+
+
+# In[13]:
+
+
+#task-3
+color = ['red','blue','red','green','red']
+print(color.count('red'))
+
+
+# In[15]:
+
+
+redd = color.remove('red')
+print(color)
+
+
+# In[10]:
+
+
+#task-4
+groceries = ['bread','milk','eggs','butter']
+print(groceries)
+groceries.append('cheese')
+print(groceries)
+groceries.remove('milk')
+print(groceries)
+groceries.insert(1,'yogurt')
+print(groceries)
+
+
+# In[ ]:
+
+
+
+
