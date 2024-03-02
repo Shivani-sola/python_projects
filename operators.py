bc4c60886e69cf3b7d7338562148534002017c15
Shivani-sola <149178251+Shivani-sola@users.noreply.github.com>
2024-03-02 20:21:46 +0530

    Add files via upload
    operators  with example.

---
+++ operators.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # TASK-1
+
+# In[6]:
+
+
+num1 = 10
+num2 = 20
+add = num1 + num2
+print(add)
+add2 = num2-num1
+print(add2)
+add3 = num1*num2
+print(add3)
+add4 =num1/num2
+print(add4)
+add5 =num1%num2
+print(add5)
+add6 =num1**num2
+print(add6)
+
+
+# # task-2
+
+# In[7]:
+
+
+is_sunny = True
+is_warm = False
+if is_sunny and is_warm:
+    print("It's a nice day!")
+else:
+    print("It's not a nice day!")
+if is_sunny or is_warm:
+    print("It's not too bad!")
+else:
+    print("It's terrible!")
+if not is_sunny:
+    print("It's cloudy!")
+else:
+    print("It's sunny!")
+
+
+# # task-3
+
+# In[9]:
+
+
+a = 10
+b = 10
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+a = 10
+b = 20    
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+if a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is not less than or equal to b")
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("a is not greater than or equal to b")
+
+
+# # task-4
+
+# In[10]:
+
+
+first_name = 'shivani'
+last_name ='sola'
+full_name = first_name +" "+last_name
+print(full_name)
+
+
+# In[ ]:
+
+
+name = 'innomatics'
+print(name * 5)
+
+
+# In[1]:
+
+
+X =5
+X+=4
+X-=2
+print(X)
+
+
+# In[2]:
+
+
+list1 = [10,20,30]
+loist2 =[10,20,30]
+print(list1 is loist2)
+
+
+# In[3]:
+
+
+num =5
+(num>=5) and (num!=5)
+
+
+# In[5]:
+
+
+print(type(10/5))
+print(type(7//2))
+
+
+# In[6]:
+
+
+s ="Innomatics"
+res1 ="matic" in s
+print(res1)
+res2 ="inno" in s
+print(res2)
+print(res1 or res2)
+
+
+# In[8]:
+
+
+num = 4+5/5*2
+print(type(num))
+
+
+# In[ ]:
+
+
+
+
